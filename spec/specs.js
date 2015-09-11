4134222ad5e0cebf1df17f91d324680449949d1f
cdg77 <cdegregorio@gmail.com>
2015-09-11 16:53:02 -0700

    return false if the word is not contained within the string. 3 specs pass.

--- spec/specs.js
+++ spec/specs.js
@@ -4,12 +4,16 @@ describe('findAndReplace', function() {
     expect(findAndReplace('Yeah. The coversheet. I know, I know. Uh, Bill talked to me about it.')).to.equal('Yeah. The coversheet. I know, I know. Uh, Bill talked to me about it.');
   });
 
-  it.skip('returns true if the strings match', function() {
-    expect(findAndReplace('Yeah. The coversheet. I know, I know. Uh, Bill talked to me about it.')).to.equal(true);
+  it('returns true if the strings match', function() {
+    expect(findAndReplace('Yeah. The coversheet. I know, I know. Uh, Bill talked to me about it.', 'Yeah. The coversheet. I know, I know. Uh, Bill talked to me about it.')).to.equal(true);
   });
 
   it('returns true if the string contains a specified phrase', function() {
     expect(findAndReplace('Yeah. The coversheet. I know, I know. Uh, Bill talked to me about it.', 'coversheet')).to.equal(true);
+  });
+
+  it('returns false if the string does not contain the specified phrase', function() {
+    expect(findAndReplace('Short ribs andouille pig shankle, spare ribs anim enim eu dolore deserunt.', 'grains')).to.equal(false);
   });
 
 });
